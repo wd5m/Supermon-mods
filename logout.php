016da7b4b53a3b968e271c6d5d5ff919f8a1d6eb
David McAnally <wd5m@users.noreply.github.com>
2022-11-07 13:44:54 -0600

    Create logout.php
    Adds logging when authenticated user logs out.

---
+++ logout.php
@@ -0,0 +1,15 @@
+<?php
+        // New php session management code -- KB4FXC 01/25/2018
+        include("session.inc");
+        openlog("SUPERMON",LOG_ODELAY , LOG_LOCAL0);    // WD5M 20221107 - Log to Linux syslog
+        error_log($_SESSION['user'].' logout succeeded.');      // WD5M 20221107 - Log to Apache error.log
+        syslog(LOG_INFO,$_SESSION['user'].' logout succeeded.');        // WD5M 20221107 - Log to Linux syslog
+        closelog();     // WD5M 20221107 - Log to Linux syslog
+        session_unset();
+        $_SESSION['sm61loggedin'] = false;
+
+        //`echo logout called >> /tmp/log.out`;
+
+
+        print "Logged out.";
+?>
