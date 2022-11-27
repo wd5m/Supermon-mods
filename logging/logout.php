d89440ac406a6234c190324477fd8258cab61c3f
David McAnally <wd5m@users.noreply.github.com>
2022-11-27 08:42:57 -0600

    Update logout.php
    Add $_SERVER['REMOTE_ADDR'] to log user IP address.

--- logging/logout.php
+++ logging/logout.php
@@ -2,8 +2,8 @@
         // New php session management code -- KB4FXC 01/25/2018
         include("session.inc");
         openlog("SUPERMON",LOG_ODELAY , LOG_LOCAL0);    // WD5M 20221107 - Log to Linux syslog
-        error_log($_SESSION['user'].' logout succeeded.');      // WD5M 20221107 - Log to Apache error.log
-        syslog(LOG_INFO,$_SESSION['user'].' logout succeeded.');        // WD5M 20221107 - Log to Linux syslog
+        error_log($_SERVER['REMOTE_ADDR'].' '.$_SESSION['user'].' logout succeeded.');      // WD5M 20221107 - Log to Apache error.log
+        syslog(LOG_INFO,$_SERVER['REMOTE_ADDR'].' '.$_SESSION['user'].' logout succeeded.');        // WD5M 20221107 - Log to Linux syslog
         closelog();     // WD5M 20221107 - Log to Linux syslog
         session_unset();
         $_SESSION['sm61loggedin'] = false;
